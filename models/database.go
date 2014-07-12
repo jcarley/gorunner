a71cca9ddcf544d349fd1351b63ea6bf3c647957
jcarley <jcarley@gettyimages.com>
2014-07-12 07:48:07 -0500

    Formatting updates enforced by go fmt

--- models/database.go
+++ models/database.go
@@ -1,89 +1,104 @@
 package models
 
 import (
-  "io/ioutil"
-  "os"
-  "sort"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
 )
 
 const (
-  jobsFile     = "jobs.json"
-  runsFile     = "runs.json"
-  tasksFile    = "tasks.json"
-  triggersFile = "triggers.json"
+	// jobsFile     = "jobs.json"
+	runsFile     = "runs.json"
+	tasksFile    = "tasks.json"
+	triggersFile = "triggers.json"
 )
 
 var (
-  jobList     JobList
-  taskList    TaskList
-  runList     RunList
-  triggerList TriggerList
+	// jobList     JobList
+	taskList    TaskList
+	runList     RunList
+	triggerList TriggerList
 )
 
 type ListWriter func([]byte, string)
 type ListReader func(string) []byte
 
 func InitDatabase() {
-  jobList = JobList{list{elements: make([]elementer, 10), fileName: jobsFile}}
-  taskList = TaskList{list{elements: make([]elementer, 10), fileName: tasksFile}}
-  triggerList = TriggerList{list{elements: make([]elementer, 10), fileName: triggersFile}}
-  runList = RunList{list{elements: make([]elementer, 10), fileName: runsFile}}
+	// jobList = JobList{list{elements: make([]elementer, 10), fileName: jobsFile}}
+	taskList = TaskList{list{elements: make([]elementer, 10), fileName: tasksFile}}
+	triggerList = TriggerList{list{elements: make([]elementer, 10), fileName: triggersFile}}
+	runList = RunList{list{elements: make([]elementer, 10), fileName: runsFile}}
 
-  jobList.Load()
-  taskList.Load(readFile)
-  triggerList.Load(readFile)
-  runList.Load()
+	// jobList.Load()
+	taskList.Load(readFile)
+	triggerList.Load(readFile)
+	runList.Load()
 }
 
 func GetJobList() *JobList {
-  return &jobList
+	dbContext := NewDbContext()
+
+	var jobs []Job
+	_, err := dbContext.Dbmap.Select(&jobs, "select * from jobs")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jobList := JobList{list{elements: make([]elementer, 0)}}
+
+	for _, job := range jobs {
+		jobList.elements = append(jobList.elements, job)
+	}
+
+	return &jobList
 }
 
 func GetRunList() *RunList {
-  return &runList
+	return &runList
 }
 
 func GetRunListSorted() *RunList {
-  sort.Sort(Reverse{&runList})
-  return &runList
+	sort.Sort(Reverse{&runList})
+	return &runList
 }
 
 func GetTaskList() *TaskList {
-  return &taskList
+	return &taskList
 }
 
 func GetTriggerList() *TriggerList {
-  return &triggerList
+	return &triggerList
 }
 
 func writeFile(bytes []byte, filePath string) {
-  err := ioutil.WriteFile(filePath, bytes, 0644)
-  if err != nil {
-    panic(err)
-  }
+	err := ioutil.WriteFile(filePath, bytes, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func readFile(filePath string) []byte {
-  _, err := os.Stat(filePath)
-  if err != nil {
-    println("Couldn't read file, creating fresh:", filePath)
-    err = ioutil.WriteFile(filePath, []byte("[]"), 0644)
-    if err != nil {
-      panic(err)
-    }
-  }
+	_, err := os.Stat(filePath)
+	if err != nil {
+		println("Couldn't read file, creating fresh:", filePath)
+		err = ioutil.WriteFile(filePath, []byte("[]"), 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
 
-  bytes, err := ioutil.ReadFile(filePath)
-  if err != nil {
-    panic(err)
-  }
-  return bytes
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
 }
 
 type Reverse struct {
-  sort.Interface
+	sort.Interface
 }
 
 func (r Reverse) Less(i, j int) bool {
-  return r.Interface.Less(j, i)
+	return r.Interface.Less(j, i)
 }
