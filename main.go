d89703f07e3fc1f9a85dbb7bd576ecb77805c9ab
jcarley <jcarley@gettyimages.com>
2014-07-15 16:30:46 -0500

    Adds gorilla logging handler

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
 
+	gorillahandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/jcarley/gorunner/handlers"
 	"github.com/jcarley/gorunner/hub"
@@ -20,8 +22,14 @@ var r *mux.Router
 
 // This filter enables messing with the request/response before and after the normal handler
 func filter(w http.ResponseWriter, req *http.Request) {
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := string(data)
+	log.Printf("%s %s %s %s\n", req.RemoteAddr, req.Method, req.URL, s)
+
 	r.ServeHTTP(w, req) // calls the normal handler
-	log.Printf("%s %s %s\n", req.RemoteAddr, req.Method, req.URL)
 }
 
 // TODO: Move to handlers package when more websocket handling is required.
@@ -47,7 +55,7 @@ func main() {
 	r = mux.NewRouter()
 	handlers.Install(r)
 	models.InitDatabase()
-	http.HandleFunc("/", filter)
+	http.Handle("/", gorillahandlers.LoggingHandler(os.Stdout, r))
 
 	fmt.Println("Running on " + port)
 	l, e := net.Listen("tcp", port)
